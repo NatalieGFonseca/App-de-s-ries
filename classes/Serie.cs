650c079ac67de1c5c7e3832dd0627007198f39bf
NatalieGFonseca <nataliegf2014@gmail.com>
2021-02-25 19:53:52 -0300

    criando classe série

---
+++ classes/Serie.cs
@@ -0,0 +1,10 @@
+namespace App.Series
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero genero {get; set;}
+        private string Titulo {get; set;}
+        private string Descricao {get; set;}
+        private int ano {get; set;}
+    }
+}
