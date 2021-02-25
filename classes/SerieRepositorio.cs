190e41942378c4335b3c34e75e73e8356a21b025
NatalieGFonseca <nataliegf2014@gmail.com>
2021-02-25 20:44:55 -0300

    criando classe

---
+++ classes/SerieRepositorio.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using App.Series.interfaces;
+
+namespace App.Series.classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            
+        }
+
+        public void Insere(Serie objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
