1f773bb89543deb330b5ed728395148b9279b568
NatalieGFonseca <nataliegf2014@gmail.com>
2021-02-25 20:09:31 -0300

    criando construtor

--- classes/Serie.cs
+++ classes/Serie.cs
@@ -2,9 +2,19 @@ namespace App.Series
 {
     public class Serie : EntidadeBase
     {
-        private Genero genero {get; set;}
-        private string Titulo {get; set;}
-        private string Descricao {get; set;}
-        private int ano {get; set;}
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.id = id;
+            this.genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.ano = ano;
+
+        }
+        private Genero genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int ano { get; set; }
     }
+
 }
